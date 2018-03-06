c61b312372d099594a7af7dbfb01884e0cb5fb6c
nicolasambroise <nicolasambroise@hotmail.com>
2018-03-07 00:23:21 +0100

    Fix: Bug with Service Worker
    Modifications:
    - Change link in index.html
    - Update app.js and sw.js

--- app.js
+++ app.js
@@ -1,37 +1,31 @@
 // register service worker
 window.addEventListener('load', () => {
-	console.log("app.js");
 	// Initialisation
-	//if ('serviceWorker' in navigator) {
 	if (!navigator.serviceWorker) {return;}
-    navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(
+  navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(
 		function(reg) {
-				if (!navigator.serviceWorker.controller) {return;}
-				if(reg.installing) {
-				  console.log('Service worker installing');
-				  trackInstalling(reg.installing);return;
-				} else if(reg.waiting) {
-				  console.log('Service worker installed');
-				  updateReady(reg.waiting);return;
-				} else if(reg.active) {
-				  console.log('Service worker active');
-				}
-
-				reg.addEventListener('updatefound', function() {
-					console.log('Update found !');
-					trackInstalling(reg.installing)
-				});
-	  }
-		openSocket();
-
-	}).catch(function(error) {
-		 console.log('Registration failed with ' + error);// registration failed
-	});
+			if (!navigator.serviceWorker.controller) {return;}
+			if(reg.installing) {
+			  console.log('Service worker installing');
+			  trackInstalling(reg.installing);return;
+			} else if(reg.waiting) {
+			  console.log('Service worker installed');
+			  updateReady(reg.waiting);return;
+			} else if(reg.active) {
+			  console.log('Service worker active');
+			}
+			reg.addEventListener('updatefound', function() {
+				console.log('Update found !');
+				trackInstalling(reg.installing);
+			});
+		}).catch(function(error) {
+	  	 console.log('Registration failed with ' + error);// registration failed
+	  });
 
 	  // Ensure refresh is only called once.
 	  // This works around a bug in "force update on reload".
 	  var refreshing;
-	    navigator.serviceWorker.addEventListener('controllerchange', function() {
+	  navigator.serviceWorker.addEventListener('controllerchange', function() {
 			if (refreshing) return;
 			console.log('Refreshing !');
 			window.location.reload();
@@ -39,46 +33,20 @@ window.addEventListener('load', () => {
 	  });
 });
 
-	function trackInstalling(worker) {
-	  var indexController = this;
-	  worker.addEventListener('statechange', function() {
+function trackInstalling(worker) {
+  var indexController = this;
+	worker.addEventListener('statechange', function() {
 		if (worker.state == 'installed') {
 		  updateReady(worker);
 		}
-	  });
-	};
+	});
+};
 
-	function updateReady(worker) {
-		console.log("New version available");
-		if (confirm('New version available, press OK to reload the website')) {
-			console.log({action: 'skipWaiting'});
-		} else {
-			alert('Why did you press cancel? You should have confirmed');
-		}
-
-	};
-	
-	function openSocket() {
-		  var indexController = this;
-		  var latestPostDate = this._postsView.getLatestPostDate();
-
-		  // create a url pointing to /updates with the ws protocol
-		  var socketUrl = new URL('/updates', window.location);
-		  socketUrl.protocol = 'ws';
-
-		  if (latestPostDate) {
-			socketUrl.search = 'since=' + latestPostDate.valueOf();
-		  }
-
-		  socketUrl.search += '&' + location.search.slice(1);
-		  var ws = new WebSocket(socketUrl.href);
-
-		  // add listeners
-		  ws.addEventListener('open', function() {console.log('open');});
-		  ws.addEventListener('message', function(event) {console.log(event.data);});
-		  ws.addEventListener('close', function() {
-			console.log('close');
-			// try and reconnect in 5 seconds
-			setTimeout(function() {openSocket();}, 5000);
-		  });
-		};
+function updateReady(worker) {
+	console.log("New version available");
+	if (confirm('New version available, press OK to reload the website')) {
+		console.log({action: 'skipWaiting'});
+	} else {
+		alert('Why did you press cancel? You should have confirmed');
+	}
+};
--- sw.js
+++ sw.js
@@ -1,21 +1,46 @@
+const staticCacheName = 'nico-static-resto-v1';
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
-        '/index.html'
+        '/index.html',
+        '/restaurant.html',
+        '/css/styles.css',
+        '/css/font-awesome.min.css',
+        '/fonts/CaviarDreams.ttf',
+        '/fonts/fontawesome-webfont.ttf',
+        '/img/1_100w.jpg',
+        '/img/2_100w.jpg',
+        '/img/3_100w.jpg',
+        '/img/4_100w.jpg',
+        '/img/5_100w.jpg',
+        '/img/6_100w.jpg',
+        '/img/7_100w.jpg',
+        '/img/8_100w.jpg',
+        '/img/9_100w.jpg',
+        '/img/10_100w.jpg',
+        '/js/dbhelper.js',
+        '/js/main.js',
+        '/js/restaurant_info.js',
+        '/js/script.js',
+        '/logo/BSicon_REST.png',
+        '/logo/BSicon_REST.svg',
+        'manifest.json'
 	    ]);
     })
   );
 });
 
 self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
   event.respondWith(
 	  caches.match(event.request).then(function(response) {
 		if (response !== undefined) {
 		  return response;
 		} else {
 		  return fetch(event.request).then(function (response) {
-			var responseClone = response.clone();
+			const responseClone = response.clone();
 			return response;
 		  }).catch(function () {
 			     return caches.match('');

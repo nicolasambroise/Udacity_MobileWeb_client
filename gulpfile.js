0a7de0be246482aef74610636321eafd507ed37c
nicolasambroise <nicolasambroise@hotmail.com>
2018-04-18 23:43:17 +0200

    Fix 2 Gulp Build
    Modifications:
    - Check all feature 1 by 1
    - Change uglify by uglify-es

--- gulpfile.js
+++ gulpfile.js
@@ -16,27 +16,35 @@ var dist = "./dist"; // dossier à livrer
 var gulp = require("gulp");
 var autoprefixer = require("gulp-autoprefixer");
 var sass = require("gulp-sass");
-var minifyJS = require("gulp-uglify");
-var jshint = require("gulp-jshint");
-var concatJS = require("gulp-concat");
+var concat = require("gulp-concat");
 var imagemin = require("gulp-imagemin");
 var sequence = require("run-sequence");
+var csso = require("gulp-csso");
 var clean = require("gulp-clean");
+var cache = require("gulp-cache");
 var rename = require("gulp-rename");
 var plumber = require("gulp-plumber");
 var htmlbeautify = require("gulp-html-beautify");
 var jimpresize = require("gulp-jimp-resize");
 var eslint = require("gulp-eslint");
 var notify = require("gulp-notify");
+var uglify = require('gulp-uglify-es').default; /* For ES6 ! */
 var browserSync = require("browser-sync").create();
+var jasmine = require('gulp-jasmine-phantom');
+var htmlclean = require('gulp-htmlclean');
+var htmlmin = require('gulp-htmlmin');
+//$ npm install --save-dev gulp-jasmine-phantom
+var jshint = require("gulp-jshint");
+//$  npm install jshint gulp-jshint --save-dev
 
 // Include plugins automatiquement
 var plugins = require("gulp-load-plugins")({pattern: ["gulp-*", "gulp.*"], replaceString: /\bgulp[\-.]/}); // tous les plugins de package.json
 
+
 // Server live editing
 gulp.task("serve", ["sass"], function() {
     browserSync.init({
-        server: "./app"
+        server: "./dist"
     });
     gulp.watch("scss/*.scss", ["Run_style"]);
     gulp.watch("css/*.css", ["Run_style"]);
@@ -47,28 +55,27 @@ gulp.task("serve", ["sass"], function() {
 
 
 /* ************* JS FILE ************* */
-// Concatenate JS ( Multiple JS file --> scripts.js --> scripts.min.js) -- OK
+// minify JS
+/* ==> OK */
 gulp.task("JSminify", function() {
   return gulp.src([src + "/js/*.js", "!" +src + "/js/*.min.js"])
-    .pipe(concatJS("scripts.js"))
+    .pipe(plugins.rename({suffix: ".min"}))
+    .pipe(uglify())
     .pipe(gulp.dest(dist + "/js"))
-    .pipe(plugins.notify({title: "Gulp",message: "JSconcat Done"}))
-    .pipe(plugins.uglify())
-    .pipe(rename("scripts.min.js"))
-    .pipe(gulp.dest(dist + "/js"))
-    .pipe(plugins.notify({title: "Gulp",message: "JSminify Done"}))
-    .pipe(browserSync.stream());
+    .pipe(plugins.notify({title: "Gulp",message: "JSminify Done"}));
   });
 
 // Check JS code for error with ES lint
+// Error : No ESLint configuration found.
 gulp.task("JSlint", function() {
-  return gulp.src(["**/*.js","!node_modules/**"])
+  return gulp.src([src + "/js/*.js","!node_modules/**"])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
 
-// Check JS code for errors. -- OK
+// Check JS code for errors.
+// Error : Invalid reporter
 gulp.task("JScheck", function() {
   return gulp.src([src + "/js/*.js", "!" +src + "/js/*.min.js", "!" +src + "/js/*analytics.js"]) // Exception Google Analytics
     .pipe(jshint())
@@ -76,15 +83,28 @@ gulp.task("JScheck", function() {
     .pipe(jshint.reporter("fail")); // task fails on JSHint error
   });
 
-        // Copy SW to dist -- OK
-gulp.task("JScopy", function() {
-  return gulp.src(src + "/*.js")
-    .pipe(plugins.uglify())
-    .pipe(gulp.dest(dist + "/"))
+// Copy SW to dist
+/* ==> OK  */
+gulp.task('JScopy', function (cb) {
+  return gulp.src([src + "/sw.js", src + "/app.js"])
+    .pipe(plugins.rename({suffix: ".min"}))
+    .pipe(uglify())
+    .pipe(gulp.dest(dist + "/js"))
     .pipe(plugins.notify({title: "Gulp",message: "JScopy Done"}));
-  });
+});
+
+// Unit Test JS
+/* A tester */
+gulp.task("JSUnit",function(){
+  gulp.src('tests/spec/extraSpec.js')
+  .pipe(jasmine({
+    integration: true,
+    vendor: '/js/*.js'
+  }))
+});
 
 /* ************* CSS FILE ************* */
+/* ==> OK */
 gulp.task("CSSsass", function() {
   return gulp.src(src+"/sass/*.scss")
     .pipe(plugins.sass())
@@ -94,6 +114,7 @@ gulp.task("CSSsass", function() {
   });
 
 // Concatenate CSS
+/* ==> OK */
 gulp.task("CSSconcat", function() {
   return gulp.src([src + "/css/*.css","!"+ src+"/css/*.min.css"]) // ATTENTION Exeption sur les min.css
     .pipe(plugins.concat("styles.css"))
@@ -102,6 +123,7 @@ gulp.task("CSSconcat", function() {
   });
 
 // Minify CSS ( styles.css --> styles.min.css et ???.css --> ???.min.css) -- OK
+/* ==> OK */
 gulp.task("CSSminify", function() {
   return gulp.src([dist + "/css/*.css", "!"+ dist + "/css/*.min.css"]) // ATTENTION Exeption sur les min.css
     .pipe(plugins.csso())
@@ -112,7 +134,8 @@ gulp.task("CSSminify", function() {
 
 /* ************* HTML FILE ************* */
 
-// Minify HTMLpages  -- OK
+// Minify HTMLpages
+/* ==> OK */
 gulp.task("HTMLpages", function() {
   return gulp.src(src + "/*.html")
     .pipe(plugins.htmlclean())
@@ -123,31 +146,39 @@ gulp.task("HTMLpages", function() {
 /* ************* IMAGES ************* */
 
 // Optimize .jpg picture
+/* ==> OK */
 gulp.task("IMGoptimize", function() {
-  return gulp.src([src + "/img/*.{jpg}", src + "/img/*.{webp}"])
-    .pipe(plugins.cache(plugins.imagemin({ optimizationLevel: 5, progressive: true, interlaced: true, verbose: true })))
-    .pipe(gulp.dest(dist + "/logo/"));
+  return gulp.src([src + "/img/*.jpg", src + "/img/*.webp"])
+    .pipe(imagemin([
+      imagemin.jpegtran({progressive: true}),
+    ]))
+    .pipe(gulp.dest(dist + "/img/"));
   });
 
-  // Optimize logo
+// Optimize logo
+/* ==> OK */
 gulp.task("LOGOoptimize", function() {
-  return gulp.src([src + "/logo/*.{png}",src + "/logo/*.{svg}"])
-    .pipe(plugins.cache(plugins.imagemin({ optimizationLevel: 5, progressive: true, interlaced: true, verbose: true })))
-    .pipe(gulp.dest(dist + "/logo/"));
+  return gulp.src([src + "/logo/*.png",src + "/logo/*.svg"])
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 5}),
+    imagemin.svgo({plugins:[{removeViewBox: true},{cleanupIDs: false}]})
+  ]))
+  .pipe(gulp.dest(dist + "/logo/"));
  });
 /* *************** MOVE & COPY ************* */
 
+// copy FONTS
+ /* ==> OK */
 gulp.task("FONTScopy", function() {
   return gulp.src(src + "/fonts/*")
   .pipe(gulp.dest(dist + "/fonts/"))
-  .pipe(plugins.notify({title: "Gulp",message: "FONTScopy Done"}));
 });
 
-
+// copy JSON
+/* ==> OK */
 gulp.task("JSONcopy", function() {
-  return gulp.src(src + "/*.{json}")
+  return gulp.src(src + "/*.json")
   .pipe(gulp.dest(dist + "/"))
-  .pipe(plugins.notify({title: "Gulp",message: "JSONcopy Done"}));
 });
 
 
